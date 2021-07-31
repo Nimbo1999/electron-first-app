f6044117754fa38497cbca158142ebeccec5f43f
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2021-07-31 17:08:59 -0300

    HelloWord

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Hello word')
+// Main process
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+	// Browser window <- Renderer Process
+	const window = new BrowserWindow({
+		width: 800,
+		height: 800,
+		backgroundColor: 'white',
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+
+	window.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', event => {
+
+	console.log(event);
+
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) createWindow();
+})
